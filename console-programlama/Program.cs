434e68455f0370a046273818894feeb51dae6898
frknn <frknstbs@gmail.com>
2021-08-04 00:01:08 +0300

    first commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("Adınızı Giriniz:");
+      string firstName = Console.ReadLine();
+      Console.WriteLine("Soyadınızı Giriniz:");
+      string lastName = Console.ReadLine();
+      Console.WriteLine("Merhaba, " + firstName + " " + lastName + "!");
+    }
+  }
+}
