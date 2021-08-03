846098e5a4db3e8dec15df21b17bffde82e134a5
frknn <frknstbs@gmail.com>
2021-08-04 02:36:27 +0300

    operatorler ödevi commiti

---
+++ operatorler/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace operatorler
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      // Atama ve işlemli atama
+      Console.WriteLine("---- ATAMA VE İŞLEMLİ ATAMA ----");
+
+      int x = 3;
+      int y = 3;
+
+      y = y + 2;
+
+      Console.WriteLine(y);
+      y += 2;
+      Console.WriteLine(y);
+      y /= 1;
+      Console.WriteLine(y);
+      x *= 2;
+      Console.WriteLine(x);
+
+      // Mantıksal operatörler
+      Console.WriteLine("---- MANTIKSAL ----");
+
+      bool isSuccess = true;
+      bool isCompleted = false;
+
+      if (isSuccess && isCompleted)
+      {
+        Console.WriteLine("Perfect!");
+      }
+      if (isSuccess || isCompleted)
+      {
+        Console.WriteLine("Great!");
+      }
+      if (isSuccess && !isCompleted)
+      {
+        Console.WriteLine("Fine!");
+      }
+
+      // İlişkisel Operatörler
+      Console.WriteLine("---- İLİŞKİSEL ----");
+
+      int a = 3;
+      int b = 4;
+      bool sonuc = a < b;
+      Console.WriteLine(sonuc);
+
+      sonuc = a <= b;
+      Console.WriteLine(sonuc);
+
+      sonuc = a > b;
+      Console.WriteLine(sonuc);
+
+      sonuc = a >= b;
+      Console.WriteLine(sonuc);
+
+      sonuc = a != b;
+      Console.WriteLine(sonuc);
+
+      sonuc = a == b;
+      Console.WriteLine(sonuc);
+
+      Console.WriteLine("---- ARİTMETİK ----");
+
+      int sayi1 = 10;
+      int sayi2 = 5;
+
+      int sonuc1 = sayi1 / sayi2;
+      Console.WriteLine(sonuc1);
+
+      sonuc1 = sayi1 * sayi2;
+      Console.WriteLine(sonuc1);
+
+      sonuc1 = sayi1 + sayi2;
+      Console.WriteLine(sonuc1);
+
+      sayi1++;
+      sonuc1 = sayi1;
+      Console.WriteLine(sonuc1);
+
+      int sonuc2 = 20 % 3;
+      Console.WriteLine(sonuc2);
+    }
+  }
+}
