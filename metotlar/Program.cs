c5f41022da08686873801c5f5ea2749c975a7070
frknn <frknstbs@gmail.com>
2021-08-04 16:14:44 +0300

    metotlar odev commiti

---
+++ metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace metotlar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int a = 3;
+      int b = 5;
+
+      int sonuc = Topla(a, b);
+      Metotlar ornek = new Metotlar();
+
+      ornek.EkranaYazdir(Convert.ToString(sonuc));
+      int sonuc2 = ornek.ArtirVeTopla(ref a, ref b);
+      ornek.EkranaYazdir(Convert.ToString(sonuc2));
+      ornek.EkranaYazdir(Convert.ToString(Topla(a, b)));
+
+    }
+
+    static int Topla(int a, int b)
+    {
+      return a + b;
+    }
+  }
+
+  class Metotlar
+  {
+    public void EkranaYazdir(string veri)
+    {
+      Console.WriteLine(veri);
+    }
+
+    public int ArtirVeTopla(ref int d1, ref int d2)
+    {
+      d1 += 1;
+      d2 += 1;
+      return d1 + d2;
+    }
+  }
+}
