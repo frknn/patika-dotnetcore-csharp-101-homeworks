84bbd49d2a1489bd000cb88a4fb045fa462d95a5
frknn <frknstbs@gmail.com>
2021-08-04 12:07:55 +0300

    for-loop odev commiti

---
+++ for-loop/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace for_loop
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Lütfen bir sayi giriniz: ");
+      int sayi = int.Parse(Console.ReadLine());
+      for (int i = 1; i <= sayi; i++)
+      {
+        if (i % 2 == 1)
+        {
+          Console.WriteLine(i);
+        }
+      }
+
+      int tekToplam = 0;
+      int ciftToplam = 0;
+      for (int i = 1; i <= 1000; i++)
+      {
+        if (i % 2 == 0)
+        {
+          ciftToplam += i;
+        }
+        else
+        {
+          tekToplam += i;
+        }
+      }
+      Console.WriteLine("Tek Sayilar Toplami: " + tekToplam);
+      Console.WriteLine("Cift Sayilar Toplami: " + ciftToplam);
+
+      // break ve continue
+      for (int i = 1; i < 10; i++)
+      {
+        if (i == 4)
+        {
+          break;
+        }
+        Console.WriteLine(i);
+      }
+
+      for (int i = 1; i < 10; i++)
+      {
+        if (i == 4)
+        {
+          continue;
+        }
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
