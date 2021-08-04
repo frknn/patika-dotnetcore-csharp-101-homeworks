eb7c9b2a336b94da62d632895e46b909f2d66d7e
frknn <frknstbs@gmail.com>
2021-08-04 12:56:17 +0300

    while-foreach odev commiti

---
+++ while-foreach/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace while_foreach
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Sayi Giriniz: ");
+      int sayi = int.Parse(Console.ReadLine());
+      int sayac = 1;
+      int toplam = 0;
+      while (sayac <= sayi)
+      {
+        toplam += sayac;
+        sayac++;
+      }
+      Console.WriteLine(toplam / sayi);
+
+      char c = 'a';
+      while (c <= 'z')
+      {
+        Console.Write(c);
+        c++;
+      }
+
+      // foreach
+      string[] arabalar = { "AUDI", "BMW", "FORD", "MERCEDES" };
+      foreach (string araba in arabalar)
+      {
+        Console.WriteLine(araba);
+      }
+    }
+  }
+}
