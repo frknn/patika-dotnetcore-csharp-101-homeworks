54d76757a9f2b78ad1bc9054733d07ee8ca19811
frknn <frknstbs@gmail.com>
2021-08-05 16:13:25 +0300

    enum odev commiti

---
+++ enum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace enumlar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(GUNLER.PAZARTESI);
+      Console.WriteLine((int)GUNLER.PAZAR);
+
+      int sicaklik = 23;
+      if (sicaklik <= (int)HAVA_DURUMU.NORMAL)
+      {
+        Console.WriteLine("Hava dışarı çıkmak için soğuk.");
+      }
+      else if (sicaklik >= (int)HAVA_DURUMU.SICAK)
+      {
+        Console.WriteLine("Hava dışarı çıkmak için çok sıcak.");
+      }
+      else if (sicaklik >= (int)HAVA_DURUMU.NORMAL && sicaklik < (int)HAVA_DURUMU.SICAK)
+      {
+        Console.WriteLine("Haydi dışarı çıkalım.");
+      }
+    }
+  }
+
+  enum GUNLER
+  {
+    PAZARTESI = 1,
+    SALI,
+    CARSAMBA,
+    PERSEMBE,
+    CUMA,
+    CUMARTESI = 99,
+    PAZAR
+  }
+
+  enum HAVA_DURUMU
+  {
+    SOGUK = 5,
+    NORMAL = 20,
+    SICAK = 25,
+    COK_SICAK = 30
+  }
+}
